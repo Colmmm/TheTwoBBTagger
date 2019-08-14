18722baea39d103c9ca4095b1c1235ef3068f28f
cosam <colm.wai.keat.sam@cern.ch>
2019-08-14 17:39:37 +0200

    move main.py to highest repo

---
+++ main.py
@@ -0,0 +1,21 @@
+from thetwobbtagger.dataobjects import twoBBdf
+from thetwobbtagger.stages import firstStage, secondStage, thirdStage
+from thetwobbtagger.lof import LOF, combine
+from thetwobbtagger.names_dict import TB_dict, ET_dict
+path = '../TaggingJpsiK2012_tiny_fix_fix.root'
+
+
+def main():
+    TBs = twoBBdf(path=path, dict=TB_dict)
+    promisingTBs = firstStage(TBs, threshold=0.02, random_seed=42)
+    
+    ETs = twoBBdf(path=path, dict=ET_dict, specific_TBs=promisingTBs.index )
+    promisingETs = secondStage(ETs, threshold=0.5, random_seed=42) ; ETs.specific_ETs = promisingETs.index
+    
+    TAG_df = combine(TB_COM_df=LOF(TBs), ET_COM_df=LOF(ETs))
+    TAGs = thirdStage(TAG_df, TB_scores=promisingTBs, path=path, random_seed=42)
+    return TAGs
+
+
+if __name__ == '__main__':
+    tags = main()

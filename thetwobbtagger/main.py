3bc2b731b00c6c88ebb263a3f92bb8b3d1e88e48
cosam <colm.wai.keat.sam@cern.ch>
2019-08-19 09:49:25 +0200

    to fix bug, gunna produce tag df to skip steps and make fixing bug easier

--- thetwobbtagger/main.py
+++ thetwobbtagger/main.py
@@ -9,14 +9,15 @@ test_path = '../TaggingJpsiK2012_tiny_fix_fixSlice2.root'
 def main():
     trainTBs = twoBBdf(path=train_path, dict=TB_dict)
     testTBs = twoBBdf(path=test_path, dict=TB_dict)
-    trainpromisingTBs, testpromisingTBs = firstStage(train_TBs=trainTBs, test_TBs=testTBs, threshold=0.01, random_seed=42)
+    trainpromisingTBs, testpromisingTBs = firstStage(train_TBs=trainTBs, test_TBs=testTBs, threshold=0.16, random_seed=42)
     
     trainETs = twoBBdf(path=train_path, dict=ET_dict, specific_TBs=trainpromisingTBs.index)
     testETs = twoBBdf(path=test_path, dict=ET_dict, specific_TBs=testpromisingTBs.index)
-    trainpromisingETs, testpromisingETs = secondStage(train_ETs=trainETs, test_ETs=testETs, threshold=0.1, random_seed=42)
+    trainpromisingETs, testpromisingETs = secondStage(train_ETs=trainETs, test_ETs=testETs, threshold=0.5, random_seed=42)
     trainETs.specific_ETs = trainpromisingETs.index ; testETs.specific_ETs = testpromisingETs.index
     
     trainTAG_df = combine(TB_COM_df=LOF(trainTBs), ET_COM_df=LOF(trainETs)) ; testTAG_df = combine(TB_COM_df=LOF(testTBs), ET_COM_df=LOF(testETs))
+    trainTAG_df.to_csv('trainTAG_df.csv') ; testTAG_df.to_csv('testTAG_df.csv')
     TAGs = thirdStage(train_TAG_df=trainTAG_df, test_TAG_df=testTAG_df, train_TB_scores=trainpromisingTBs, test_TB_scores=testpromisingTBs,
                       train_path=train_path, test_path= test_path, random_seed=42)
     return TAGs

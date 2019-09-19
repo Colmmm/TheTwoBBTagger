3901109105cf60b427823843f0c559218289a390
cosam <colm.wai.keat.sam@cern.ch>
2019-09-19 11:29:29 +0200

    saving final outputted tags, set tb threshold to 0.165, output for graphes

--- thetwobbtagger/main.py
+++ thetwobbtagger/main.py
@@ -2,6 +2,7 @@ from dataobjects import twoBBdf
 from stages import firstStage, secondStage, thirdStage
 from lof import LOF, combine
 from names_dict import TB_dict, ET_dict
+
 import gc ; gc.enable()
 train_path = '../TaggingJpsiK2012_fix_fix_1_withLOF.root'
 test_path = '../TaggingJpsiK2012_tiny_fix_fix_withLOF.root'
@@ -10,12 +11,12 @@ def main():
     # 1) TWO BODYS (TBs)
     trainTBs = twoBBdf(path=train_path, dict=TB_dict)
     testTBs = twoBBdf(path=test_path, dict=TB_dict)
-    TB_train_df, TB_test_df, TB_train_scores, test_TB_scores = firstStage(train_TBs=trainTBs, test_TBs=testTBs, threshold=0.05, random_seed=42, chunk_size=200000)
+    TB_train_df, TB_test_df, TB_train_scores, test_TB_scores = firstStage(train_TBs=trainTBs, test_TBs=testTBs, threshold=0.165, random_seed=42, chunk_size=200000)
     
     # 2) EXTRA TRACKS (ETs)
     trainETs = twoBBdf(path=train_path, dict=ET_dict, specific_TBs=TB_train_df.index)
     testETs = twoBBdf(path=test_path, dict=ET_dict, specific_TBs=TB_test_df.index)
-    ET_train_df, ET_test_df = secondStage(train_ETs=trainETs, test_ETs=testETs, threshold=0.2, random_seed=42, chunk_size=100000)
+    ET_train_df, ET_test_df = secondStage(train_ETs=trainETs, test_ETs=testETs, threshold=0.25, random_seed=42, chunk_size=100000)
     del trainTBs, testTBs, trainETs, testETs ; gc.collect()
 
     # 2.5) Combine TBs and ETs and apply LOF calculation
@@ -26,7 +27,9 @@ def main():
     # 3) LOF, combine TBs+ETs and then feed into tagger
     TAGs = thirdStage(train_TAG_df=TAG_train_df, test_TAG_df=TAG_test_df, train_TB_scores=TB_train_scores, test_TB_scores=test_TB_scores,
                       train_path=train_path, test_path= test_path, random_seed=42)
+
     return TAGs
 
 if __name__ == '__main__':
     tags = main()
+    tags.to_csv('FINAL_outputted_TAGS.csv')
